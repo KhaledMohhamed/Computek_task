59442f733a3226c81fc5eda98c3edbb47eed7e09
KhaledMohhamed <bebo.khaled28@gmail.com>
2017-11-21 17:08:34 +0200

    task Angularjs

---
+++ app/view1/view1.js
@@ -0,0 +1,39 @@
+'use strict';
+
+angular.module('myApp.view1', ['ui.router'])
+
+
+.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+        .state('home', {
+            url: '/',
+            templateUrl: 'view1/view1.html',
+            controller: 'View1Ctrl'
+        })
+        .state('user', {
+            url: 'user/:id',
+            templateUrl: 'view2/view2.html',
+            parent:'home',
+            controller: 'View2Ctr2'
+        });
+
+}])
+.controller('View1Ctrl', function($scope, $http , $state) {
+    $http.get("https://api.github.com/users")
+        .then(function(response) {
+            $scope.users = response.data;
+            $state.go('user', { "id": 1});
+        });
+})
+.controller('View2Ctr2',function($scope,$stateParams,$http,$state) {
+    $http.get("https://api.github.com/user/"+$stateParams.id)
+    .then(function(response) {
+        console.log(response.data);
+    })
+    .catch(function (data) {
+        $state.go('user', { "id": 1});
+    }) ;
+
+});
+
---
+++ app/view2/view2.js
@@ -0,0 +1,14 @@
+// 'use strict';
+//
+// angular.module('myApp.view2', ['ui.router'])
+//
+// .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+//   console.log('hello');
+//         $urlRouterProvider.otherwise('/');
+//     $stateProvider
+//         .state('user', {
+//             url: 'user/:id',
+//             templateUrl: 'view2/view2.html',
+//             controller: 'View2Ctr2'
+//         });
+// }])
